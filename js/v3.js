d43df0fc9c466ab7a64f08f6254c3c74965b100e
Zhi <yfwz100@yeah.net>
2016-02-24 20:31:48 +0800

    Enable header scrolling when content overflows.

--- js/v3.js
+++ js/v3.js
@@ -8,25 +8,27 @@ var $doc = $(document), $win = $(window);
 var $head = $('[data-headroom]');
 var pinnedClass = 'pinned';
 $doc.on('mousewheel', function (event) {
-  var evt = event.originalEvent;
-  if (!$head.hasClass(pinnedClass)) {
-    if (evt.deltaY > 0) {
-      $head.addClass(pinnedClass);
-      $head.data('pinned-ts', +new Date());
-      $doc.scrollTop(0);
-      event.preventDefault();
-    }
-  } else {
-    if (evt.deltaY < 0) {
-      if ($doc.scrollTop() + evt.deltaY <= 0) {
-        $head.removeClass(pinnedClass);
+  if ($doc.height() > $win.height()) {
+    var evt = event.originalEvent;
+    if (!$head.hasClass(pinnedClass)) {
+      if (evt.deltaY > 0) {
+        $head.addClass(pinnedClass);
+        $head.data('pinned-ts', +new Date());
         $doc.scrollTop(0);
         event.preventDefault();
       }
     } else {
-      var lastAttampt = parseInt($head.data('pinned-ts')) || 0;
-      if (new Date() - lastAttampt < 300) {
-        event.preventDefault();
+      if (evt.deltaY < 0) {
+        if ($doc.scrollTop() + evt.deltaY <= 0) {
+          $head.removeClass(pinnedClass);
+          $doc.scrollTop(0);
+          event.preventDefault();
+        }
+      } else {
+        var lastAttampt = parseInt($head.data('pinned-ts')) || 0;
+        if (new Date() - lastAttampt < 300) {
+          event.preventDefault();
+        }
       }
     }
   }
